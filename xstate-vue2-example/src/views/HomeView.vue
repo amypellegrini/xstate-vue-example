eecec281c4b0675b24141ee0efbd9b447ecc1af8
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-05-05 14:56:46 +0200

    Setup Xstate example

--- xstate-vue2-example/src/views/HomeView.vue
+++ xstate-vue2-example/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import { useMachine } from 'xstate-vue2'
+import { createMachine } from 'xstate'
+
+const toggleMachine = createMachine({
+  id: 'toggle',
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: { TOGGLE: 'active' }
+    },
+    active: {
+      on: { TOGGLE: 'inactive' }
+    }
+  }
+})
+
+const { state, send } = useMachine(toggleMachine)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <button @click="send('TOGGLE')">
+      {{ state.value === 'inactive' ? 'Click to activate' : 'Active! Click to deactivate' }}
+    </button>
   </main>
 </template>
